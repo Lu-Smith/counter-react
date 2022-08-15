2def1e727f114a44268b3ce626f797802a3cf24d
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2022-08-15 19:33:48 +0100

    Updated Counter.jsx
    Added Counter.css
    Removed App.css

--- src/Counter/Counter.jsx
+++ src/Counter/Counter.jsx
@@ -1,14 +1,21 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
+import "./Counter.css";
 
 const Counter = () => {
 
     const [counterValue, setCounterValue] = useState("0");
+    const [inputValue, setInputValue] = useState("1");
 
   return (
-    <div>
-       <h2 data-testid="header">My Counter</h2>
+    <div className='Counter'>
+       <h1 data-testid="header">My Counter</h1>
        <h2 data-testid="counter">{counterValue}</h2>
        <button data-testid="substract-btn">-</button>
+       <input 
+          type="number" 
+          data-testid="input" 
+          value={inputValue}/>
+       <button data-testid="add-btn">+</button>
     </div>
   )
 }
